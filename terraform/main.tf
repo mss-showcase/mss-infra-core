be77712b6eca5f7ad8c83bd735ef5ee5d2636072
Malyik József <jozsef.malyik@barre.hu>
2025-06-08 22:46:40 +0200

    adding a secondary bucket for runtime data

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,24 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_s3_bucket" "shared_data" {
+  bucket = var.shared_data_bucket
+  tags = {
+    Name    = "mss-s3bucket-shared-data"
+    Project = "mss"
+  }
+}
+
 resource "aws_s3_bucket" "shared_build_data" {
   bucket = var.shared_build_data_bucket
   tags = {
     Name    = "mss-s3bucket-shared-build-data"
     Project = "mss"
   }
+}
+
+output "shared_data_bucket" {
+  value = aws_s3_bucket.shared_data.id
 }
 
 output "shared_build_data_bucket" {
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,1 +1,2 @@
+shared_data_bucket = "mss-s3bucket-shared-data"
 shared_build_data_bucket = "mss-s3bucket-shared-build-data"
