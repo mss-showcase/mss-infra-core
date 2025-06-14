29524aaed09e050a389e83f5c34bee3de265bbf2
Malyik József <jozsef.malyik@barre.hu>
2025-06-14 20:50:00 +0200

    configuring webhosting bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,37 @@ resource "aws_s3_bucket" "shared_build_data" {
   }
 }
 
+resource "aws_s3_bucket_policy" "shared_data_public_read" {
+  bucket = aws_s3_bucket.shared_data.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+          "s3:GetObject"
+        ]
+        Resource = "${aws_s3_bucket.shared_data.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "shared_data_website" {
+  bucket = aws_s3_bucket.shared_data.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
 output "shared_data_bucket" {
   value = aws_s3_bucket.shared_data.id
 }
