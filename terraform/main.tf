bb339277e77c39f60c92a5256e8d0f687e0794d3
Malyik József <jozsef.malyik@barre.hu>
2025-07-05 12:49:20 +0200

    removing webhosting from webapp bucket (for the sake of cloudfront

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,45 +26,12 @@ resource "aws_s3_bucket" "webhosting" {
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "webhosting" {
-  bucket = aws_s3_bucket.webhosting.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html" 
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "webhosting" {
   bucket                  = aws_s3_bucket.webhosting.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "webhosting_public_read" {
-  bucket = aws_s3_bucket.webhosting.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = [
-          "s3:GetObject"
-        ]
-        Resource = "${aws_s3_bucket.webhosting.arn}/*"
-      }
-    ]
-  })
-
-  depends_on = [aws_s3_bucket_public_access_block.webhosting]
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 output "shared_data_bucket" {
@@ -77,8 +44,4 @@ output "shared_build_data_bucket" {
 
 output "webhosting_bucket" {
   value = aws_s3_bucket.webhosting.id
-}
-
-output "webhosting_website_endpoint" {
-  value = aws_s3_bucket_website_configuration.webhosting.website_endpoint
 }
